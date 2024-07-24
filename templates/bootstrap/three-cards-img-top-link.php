425a8568ccf16090b199624e85a77971a73fc649
Patrick <pk@konstand.de>
2024-07-24 10:07:02 +0200

    new template

---
+++ templates/bootstrap/three-cards-img-top-link.php
@@ -0,0 +1,99 @@
+<?php
+$template = [
+    'tpl' => 'bootstrap/three-cards-img-top-link.tpl',
+    'name' => 'Three Cards with image on top with a button',
+    'title' => 'Bootstrap card with image on top and text',
+    'variables' => [
+        [
+            'section' => 'Data for the first Card'
+        ],[
+            'placeholder' => 'var_img_src_one',
+            'type' => 'img_select',
+            'text' => 'Select an image'
+        ],[
+            'placeholder' => 'var_img_alt_one',
+            'type' => 'text',
+            'text' => 'Alt text for image'
+        ],[
+            'placeholder' => 'var_title_one',
+            'type' => 'text',
+            'text' => 'Title in the card body'
+        ],[
+            'placeholder' => 'var_text_one',
+            'type' => 'html',
+            'text' => 'Text in the card body'
+        ],[
+            'placeholder' => 'var_btn1_href',
+            'type' => 'text',
+            'text' => 'URL/Link for the first Button'
+        ],[
+            'placeholder' => 'var_btn1_text',
+            'type' => 'text',
+            'text' => 'Text for the first Button'
+        ],[
+            'placeholder' => 'var_btn1_class',
+            'type' => 'text',
+            'text' => 'Class for the first Button (btn-primary, btn-secondary, btn-info ...'
+        ],[
+            'section' => 'Here starts the second Card'
+        ],[
+            'placeholder' => 'var_img_src_two',
+            'type' => 'img_select',
+            'text' => 'Select an image'
+        ],[
+            'placeholder' => 'var_img_alt_two',
+            'type' => 'text',
+            'text' => 'Alt text for image'
+        ],[
+            'placeholder' => 'var_title_two',
+            'type' => 'text',
+            'text' => 'Title in the card body'
+        ],[
+            'placeholder' => 'var_text_two',
+            'type' => 'html',
+            'text' => 'Text in the card body'
+        ],[
+            'placeholder' => 'var_btn2_href',
+            'type' => 'text',
+            'text' => 'URL/Link for the first Button'
+        ],[
+            'placeholder' => 'var_btn2_text',
+            'type' => 'text',
+            'text' => 'Text for the first Button'
+        ],[
+            'placeholder' => 'var_btn2_class',
+            'type' => 'text',
+            'text' => 'Class for the first Button (btn-primary, btn-secondary, btn-info ...'
+        ],[
+            'section' => 'And the last one'
+        ],[
+            'placeholder' => 'var_img_src_three',
+            'type' => 'img_select',
+            'text' => 'Select an image'
+        ],[
+            'placeholder' => 'var_img_alt_three',
+            'type' => 'text',
+            'text' => 'Alt text for image'
+        ],[
+            'placeholder' => 'var_title_three',
+            'type' => 'text',
+            'text' => 'Title in the card body'
+        ],[
+            'placeholder' => 'var_text_three',
+            'type' => 'html',
+            'text' => 'Text in the card body'
+        ],[
+            'placeholder' => 'var_btn3_href',
+            'type' => 'text',
+            'text' => 'URL/Link for the first Button'
+        ],[
+            'placeholder' => 'var_btn3_text',
+            'type' => 'text',
+            'text' => 'Text for the first Button'
+        ],[
+            'placeholder' => 'var_btn3_class',
+            'type' => 'text',
+            'text' => 'Class for the first Button (btn-primary, btn-secondary, btn-info ...'
+        ]
+    ]
+];
---
+++ templates/bootstrap/three-cards-img-top-link.tpl
@@ -0,0 +1,32 @@
+<div class="row">
+    <div class="col-md-4">
+<div class="card h-100">
+    <img src="{var_img_src_one}" class="card-img-top" alt="{var_img_alt_one}">
+    <div class="card-body">
+        <h5 class="card-title">{var_title_one}</h5>
+        <p class="card-text">{var_text_one}</p>
+        <a href="{var_btn1_href}" class="{var_btn1_class}">{var_btn1_text}</a>
+    </div>
+</div>
+    </div>
+    <div class="col-md-4">
+        <div class="card h-100">
+            <img src="{var_img_src_two}" class="card-img-top" alt="{var_img_alt_two}">
+            <div class="card-body">
+                <h5 class="card-title">{var_title_two}</h5>
+                <p class="card-text">{var_text_two}</p>
+                <a href="{var_btn2_href}" class="{var_btn2_class}">{var_btn2_text}</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card h-100">
+            <img src="{var_img_src_three}" class="card-img-top" alt="{var_img_alt_three}">
+            <div class="card-body">
+                <h5 class="card-title">{var_title_three}</h5>
+                <p class="card-text">{var_text_three}</p>
+                <a href="{var_btn3_href}" class="{var_btn3_class}">{var_btn3_text}</a>
+            </div>
+        </div>
+    </div>
+</div>
